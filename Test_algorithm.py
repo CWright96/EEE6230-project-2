cb74b4641cf1c2bcadcefac86d1a55fe7786223c
CWright96 <34516895+CWright96@users.noreply.github.com>
2017-12-19 15:46:18 +0000

    test algorithm finished

--- Test_algorithm.py
+++ Test_algorithm.py
@@ -22,7 +22,33 @@ def test_golub_reinsch_svd(fun, A, eps = 1e-8):
         return False
 
     # check if U*U^T = I
+    if np.sum(np.abs(np.eye(A.shape[1])-np.dot(U,U.T)))> eps:
+        print("U.U.T identity test Failed!")
+        return False
     
     # check if V*V^T = I
+    if np.sum(np.abs(np.eye(A.shape[1])-np.dot(V,V.T)))> eps:
+        print("V.V.T identity test Failed!")
+        return False    
     
-    # check if B is diagonal with positive elements 
+    # check if B is diagonal with positive elements
+    
+    B_copy = np.array(B, copy = True)#create a copy of the B Matrix
+            
+    B_copy = (B_copy - np.diag(np.diagonal(B_copy)))#remove the diagonal values
+    #all other values in the matrix should be 0
+
+    for i in np.nditer(B_copy): #iterate over the array            
+        if np.absolute(i) > eps:#the value i is an element in the array
+            print("Diagonal test failed")
+            return False     #if i is greater than eps
+            break               #the matrix is not diagonal, exit loop
+    #if the test makes it this far the matrix is diagonal
+    for i in np.nditer(np.diagonal(B_copy)):
+        if i < 0:
+            print("element is not positive")
+            return False
+    #all tests passed
+    
+    return True
+  
